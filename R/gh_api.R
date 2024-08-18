65a545ae1224bfba41d49ef0e5a610ac6230631f
alex-vand <a.vandoornewaard@outlook.com>
2024-08-18 21:08:33 +0200

    Adding function doc/ code comments.
    replaced purrr:map with base R lapply in wrapper fnc.

--- R/gh_api.R
+++ R/gh_api.R
@@ -1,14 +1,23 @@
-# Generic gh GitHub API GET query
+#' @description
+#' GitHub REST API GET query function using gh package.
+#' 
+#' @param owner         A character vector of length 1 for the owner of the repo.         
+#' @param repo          A character vector of length 1 for the name of the repo.
+#' @param endpoint      A character vector of length 1 for the endpoint to be queried. 
+#' 
+#' @return              A list object containing the API query response data.
 api_get_endpoint <- function(owner, repo, endpoint) {
 
-  # Create API endpoint string
+  # Create API query to string
   api_query <- paste0("GET /repos/", owner, "/", repo, "/", endpoint)
   logger::log_debug(api_query)
 
-  # Query using gh
+  # Make GitHub API request for endpoint. tryCatch used as error handler wrapper.
   response <- tryCatch({
+    # API query
     gh::gh(api_query, owner = owner, repo = repo)
   }, error = function(e) {
+    # Logger with issue-specific error message  
     logger::log_error("Error fetching data from GitHub API for endpoint '", endpoint, "': ", e$message)
     return(NULL)
   })
@@ -16,9 +25,22 @@ api_get_endpoint <- function(owner, repo, endpoint) {
   return(response)
 }
 
-# Wrapper for multiple endpoints by repo (owner) using map
+#' @description
+#' Wrapper function for querying multiple API endpoints for a single repo.
+#'
+#' @param owner         A character vector of length 1 for the owner of the repo.
+#' @param repo          A character vector of length 1 for the name of the repo.
+#' @param endpoint      A a list of character strings for endpoints to be queried.
+#'
+#' @return              A list object containing the API query response data for each endpoint.
 api_get_endpoints <- function(owner, repo, endpoints) {
-  responses <- purrr::map(endpoints, function(endpoint) api_get_endpoint(owner, repo, endpoint))
+
+  # Query each endpoint in endpoints via call to api_get_endpoint.
+  responses <- lapply(endpoints, function(endpoint) api_get_endpoint(owner, repo, endpoint))
+  
+  # Assign endpoint names to corresponding response data.
   names(responses) <- endpoints
+
   return(responses)
+
 }
